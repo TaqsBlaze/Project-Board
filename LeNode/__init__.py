578851f21f0fa3fdc3c3aff2db8725b2e2159bbc
Taqs Blaze <tanakah30@gmail.com>
2020-01-19 10:07:11 +0200

    changed heading and corrected a few css styles

--- LeNode/__init__.py
+++ LeNode/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 
 App = Flask(__name__)
 App.config['SECRET_KEY'] = '2582ec1d875514668349f162e3b4e71c65c1'
-App.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///info.db'
+App.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/info.db'
 db = SQLAlchemy(App)
 bcrypt = Bcrypt(App)
 from LeNode.Routes import routes
